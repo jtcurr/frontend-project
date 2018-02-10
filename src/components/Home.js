c9012e6e88c2929b96dd6fbd7d7385301e8b7357
jtcurr <johntcurrie@gmail.com>
2018-02-10 11:02:37 -0800

    Building out fetching function and added a 'Loading' div if the data has not returned

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,35 @@
 import React, { Component } from 'react';
+import { fetchLatestTweets } from '../shared/utils';
 
 class Home extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      isLoaded: false,
+      tweetData: []
+    }
+    this.tweets = ['@Trizic', '@laughingsquid', '@techcrunch'];
+    this.fetchLatestTweets = fetchLatestTweets.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchLatestTweets(this.tweets);
   }
 
   render() {
-    return (
-      <div>
-      </div>
-    );
+    if (this.state.isLoaded) {
+      return (
+        <div>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          Loading...
+        </div>
+      )
+    }
+    
   }
 }
 
---
+++ src/shared/utils.js
@@ -0,0 +1,16 @@
+export async function fetchLatestTweets(twitterHandles) {
+  let tweetStorageArray = [];
+  //Boiling-harbor is a Node server setup to handle the call to the Twitter API
+  const getUserTweets = handle => fetch('https://boiling-harbor-67625.herokuapp.com/twitter?name=' + handle)
+    .then(res => res.json())
+    .then(tweets =>  {
+      tweetStorageArray = tweetStorageArray.concat(JSON.parse(tweets.data))
+    })
+
+  Promise
+    .all(twitterHandles.map(getUserTweets))
+    .then(() => this.setState({
+      tweetData: tweetStorageArray,
+      isLoaded: true
+    }))
+}
